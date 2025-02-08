8973ae8d3687953bd757eba602179c99802374fe
quazistellar <gni7777@yandex.ru>
2025-02-09 02:20:02 +0300

    saitikkk pasheeet

---
+++ Components/AverageRatingViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using guitarshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace guitarshop.Components
+{
+    public class AverageRatingViewComponent: ViewComponent
+    {
+        private readonly AspGuiznotesContext _context;
+
+        public AverageRatingViewComponent(AspGuiznotesContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int GuitarId)
+        {
+            var reviews = await _context.Reviews.Where(r => r.GuitarId == GuitarId).ToListAsync();
+
+            double averageRating = reviews.Any() ? reviews.Average(r => r.Rating) : 0;
+
+            return View("Rating", averageRating);
+        }
+    }
+}
--- obj/Debug/net8.0/guitarshop.AssemblyInfo.cs
+++ obj/Debug/net8.0/guitarshop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("guitarshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a51f990e4c5f2ebc94b4c02da21006ce94e22fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48dd72aa2d7d9386027d161a549ac6d7d4980dc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("guitarshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("guitarshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
